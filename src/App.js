e747454c0b76cea9dbc5343e1391913440313bba
FlorentLem <florentlempro@gmail.com>
2021-01-13 23:44:02 +0100

    finished selectedmarker

--- src/App.js
+++ src/App.js
@@ -4,13 +4,27 @@ import Header from "./components/elements/Header/Header";
 import SelectedMarker from "./components/elements/SelectedMarker/SelectedMarker";
 
 const App = () => {
+  const marker = [
+    {
+      Name: "oui",
+      role: false,
+      type: "marchand"
+    },
+    {
+      farmsize: 1000,
+      rate: "Expert",
+      role: true,
+      date: "2021"
+    }
+  ];
+
   return (
     <>
       <Header />
       <Router>
         <Switch>
           <Route exact path="/">
-            <SelectedMarker role={false} />
+            <SelectedMarker marker={marker[1]} />
           </Route>
         </Switch>
       </Router>
